bde224177196472112ba6196e933e9bf241fcbed
PJ <pkatema1@sheffield.ac.uk>
2019-04-19 14:28:11 +0100

    Credentials file

--- heroku-setup.py
+++ heroku-setup.py
@@ -4,15 +4,15 @@ from boto.s3.connection import S3Connection
 import main.KahunaBot
 
 # Read the credentials file
-filename = 'credentials/changeme.txt'
+filename = 'credentials/credentials.json'
 with open(filename) as json_file:
     credentials = json.load(json_file)
 
-#get token from environ variables
+#get useful things from environ variables
 credentials["Token"] = os.environ['BotToken']
+credentials["ID"] = os.environ['ClientId']
 
 #remove original file and export new credentials with correct token!
-os.remove(filename)
 with open(filename, 'w') as outfile:
     json.dump(credentials, outfile)
 
