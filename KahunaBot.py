4c9b3ae8555c819ff96281db28a1411ca228b7ba
PJ <pkatema1@sheffield.ac.uk>
2019-04-19 13:14:57 +0100

    Added Basics
    The core of the bot has been added! So far the bot can create 
    togethertube links so you guys can watch videos! It will also say 
    "Playing with Humans" in the rich prescence on the online tab on 
    discord!

---
+++ KahunaBot.py
@@ -0,0 +1,46 @@
+# Works with Python 3.6
+import asyncio
+import aiohttp
+import json
+import requests
+from discord import Game
+from discord.ext.commands import Bot
+
+# Read the credentials file
+with open('credentials/credentials.json') as json_file:
+    credentials = json.load(json_file)
+
+
+BOT_PREFIX = credentials["BotPrefix"] # Commands will start with the symbol specified here.
+TOKEN = credentials["Token"] # Get at discordapp.com/developers/applications/me
+client = Bot(command_prefix=BOT_PREFIX)
+
+@client.command(name='totube',
+                description ="Creates a link to a Together Tube room",
+                brief = "Watch Youtube videos together",
+                aliases = ['togethertube'],
+                pass_context=True)
+
+async def together_tube(context):
+    url = requests.get("https://togethertube.com/room/create").url
+    await client.say(context.message.author.mention +', your together tube link!\n\n'+ url +'\n\n Happy viewing!')
+
+
+@client.event
+async def on_ready():
+    await client.change_presence(game=Game(name="with humans"))
+    print("Logged in as " + client.user.name)
+
+
+async def list_servers():
+    await client.wait_until_ready()
+    while not client.is_closed:
+        print("Current servers:")
+        for server in client.servers:
+            print(server.name)
+        print("---------------------")
+        await asyncio.sleep(600)
+
+
+client.loop.create_task(list_servers())
+client.run(TOKEN)
